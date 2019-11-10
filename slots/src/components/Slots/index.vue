ca34349bc300f40d21cfdd2b90381c10ebe1476e
book <bookgvi@gmail.com>
2019-11-11 00:14:30 +0300

    vue create

---
+++ slots/src/components/Slots/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <span>
+          WTF
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index'
+}
+</script>
+
+<style scoped>
+
+</style>
